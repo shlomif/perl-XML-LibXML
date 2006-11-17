0086b1e33c719d9c257efc5cd93d2a1f0f599962
pajas <pajas@9ae0c189-cd1f-4510-a509-f4891f5cf20d>
2006-11-17 18:47:25 +0000

    version bumped to 1.62

--- lib/XML/LibXML/SAX/Builder.pm
+++ lib/XML/LibXML/SAX/Builder.pm
@@ -7,7 +7,7 @@ use XML::NamespaceSupport;
 
 use vars qw ($VERSION);
 
-$VERSION = "1.61"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "1.62"; # VERSION TEMPLATE: DO NOT CHANGE
 
 sub new {
     my $class = shift;
