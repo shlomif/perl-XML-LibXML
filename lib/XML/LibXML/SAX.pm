48dff5fa9215c507b724613991e5ad139accdabf
pajas <pajas@9ae0c189-cd1f-4510-a509-f4891f5cf20d>
2009-02-06 19:09:39 +0000

    - improved structured errors reporting for errors from parser (XML context and offset are provided) - bumped version to 1.69_2

--- lib/XML/LibXML/SAX.pm
+++ lib/XML/LibXML/SAX.pm
@@ -12,7 +12,7 @@ package XML::LibXML::SAX;
 use strict;
 use vars qw($VERSION @ISA);
 
-$VERSION = "1.69_1"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "1.69_2"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use XML::LibXML;
 use XML::SAX::Base;
