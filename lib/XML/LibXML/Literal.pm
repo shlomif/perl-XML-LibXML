3b66f144fc0d762bcd674e04fe9ea38d82c64e53
Shlomi Fish <shlomif@iglu.org.il>
2012-04-30 20:33:03 +0300

    Bump the version to 1.97.

--- lib/XML/LibXML/Literal.pm
+++ lib/XML/LibXML/Literal.pm
@@ -13,7 +13,7 @@ use XML::LibXML::Number;
 use strict;
 
 use vars qw ($VERSION);
-$VERSION = "1.96"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "1.97"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use overload 
 		'""' => \&value,
