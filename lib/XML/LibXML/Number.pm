9a5577196fce826fcb8f54e477ddc8480e9ff5a1
Shlomi Fish <shlomif@shlomifish.org>
2012-05-13 21:04:34 +0300

    Bump the version number to 1.98.

--- lib/XML/LibXML/Number.pm
+++ lib/XML/LibXML/Number.pm
@@ -13,7 +13,7 @@ use XML::LibXML::Literal;
 use strict;
 
 use vars qw ($VERSION);
-$VERSION = "1.97"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "1.98"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use overload
         '""' => \&value,
--- lib/XML/LibXML/SAX.pm
+++ lib/XML/LibXML/SAX.pm
@@ -12,7 +12,7 @@ package XML::LibXML::SAX;
 use strict;
 use vars qw($VERSION @ISA);
 
-$VERSION = "1.97"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "1.98"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use XML::LibXML;
 use XML::SAX::Base;
