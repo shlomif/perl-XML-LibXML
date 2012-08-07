ebd8f35c0fc3f31802e9d32931f9db68e071a062
Shlomi Fish <shlomif@shlomifish.org>
2012-08-07 23:04:44 +0300

    Bump the release to 2.0004.

--- lib/XML/LibXML/AttributeHash.pm
+++ lib/XML/LibXML/AttributeHash.pm
@@ -7,7 +7,7 @@ use Tie::Hash;
 our @ISA = qw/Tie::Hash/;
 
 use vars qw($VERSION);
-$VERSION = "2.0003"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0004"; # VERSION TEMPLATE: DO NOT CHANGE
 
 BEGIN
 {
--- lib/XML/LibXML/SAX/Generator.pm
+++ lib/XML/LibXML/SAX/Generator.pm
@@ -14,7 +14,7 @@ use strict;
 use XML::LibXML;
 use vars qw ($VERSION);
 
-$VERSION = "2.0003"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0004"; # VERSION TEMPLATE: DO NOT CHANGE
 
 sub CLONE_SKIP {
   return $XML::LibXML::__threads_shared ? 0 : 1;
