375a117e0bec81b7344d708a0cb9e415e0844dc5
Shlomi Fish <shlomif@shlomifish.org>
2012-10-17 19:02:49 +0200

    Bump the version number to 2.0007.

--- lib/XML/LibXML/Reader.pm
+++ lib/XML/LibXML/Reader.pm
@@ -14,7 +14,7 @@ use strict;
 use warnings;
 
 use vars qw ($VERSION);
-$VERSION = "2.0006"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0007"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use 5.008_000;
 
