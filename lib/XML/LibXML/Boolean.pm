057c78b4c1924a5a0940aadd12189ca720c22cfd
Shlomi Fish <shlomif@shlomifish.org>
2012-10-17 19:02:49 +0200

    Bump the version number to 2.0007.

--- lib/XML/LibXML/Boolean.pm
+++ lib/XML/LibXML/Boolean.pm
@@ -15,7 +15,7 @@ use strict;
 
 use vars qw ($VERSION);
 
-$VERSION = "2.0006"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0007"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use overload
         '""' => \&value,
--- lib/XML/LibXML/Devel.pm
+++ lib/XML/LibXML/Devel.pm
@@ -12,7 +12,7 @@ use warnings;
 use XML::LibXML;
 
 use vars qw ($VERSION);
-$VERSION = "2.0006"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0007"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use 5.008_000;
 
