523cdbb46d4743aad5feb5a05f704da8c94824b3
Shlomi Fish <shlomif@shlomifish.org>
2013-05-09 11:06:33 +0300

    Requiring libxml2-2.9.0.

--- lib/XML/LibXML/Devel.pm
+++ lib/XML/LibXML/Devel.pm
@@ -12,7 +12,7 @@ use warnings;
 use XML::LibXML;
 
 use vars qw ($VERSION);
-$VERSION = "2.0016"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0017"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use 5.008_000;
 
