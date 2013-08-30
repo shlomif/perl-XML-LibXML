657dbab122215e0f0768f19b8dd460feff9e247c
Shlomi Fish <shlomif@shlomifish.org>
2013-08-30 12:37:23 +0300

    Bump version number to 2.0104

--- lib/XML/LibXML/Boolean.pm
+++ lib/XML/LibXML/Boolean.pm
@@ -15,7 +15,7 @@ use strict;
 
 use vars qw ($VERSION);
 
-$VERSION = "2.0103"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0104"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use overload
         '""' => \&value,
--- lib/XML/LibXML/SAX.pm
+++ lib/XML/LibXML/SAX.pm
@@ -12,7 +12,7 @@ package XML::LibXML::SAX;
 use strict;
 use vars qw($VERSION @ISA);
 
-$VERSION = "2.0103"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0104"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use XML::LibXML;
 use XML::SAX::Base;
