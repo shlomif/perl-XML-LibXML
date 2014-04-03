3ae34f07d1fa89d451e523271d5de4901c378a60
Shlomi Fish <shlomif@shlomifish.org>
2014-04-03 15:58:06 +0300

    Bump version to 2.0114.

--- lib/XML/LibXML/SAX/Builder.pm
+++ lib/XML/LibXML/SAX/Builder.pm
@@ -21,7 +21,7 @@ sub CLONE_SKIP {
   return $XML::LibXML::__threads_shared ? 0 : 1;
 }
 
-$VERSION = "2.0113"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0114"; # VERSION TEMPLATE: DO NOT CHANGE
 
 sub new {
     my $class = shift;
