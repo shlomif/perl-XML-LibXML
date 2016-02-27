677f58f70e744a55c5e8c92e9a8445dba945d70d
Shlomi Fish <shlomif@shlomifish.org>
2016-02-27 13:20:04 +0200

    Bump version number.

--- lib/XML/LibXML/SAX/Parser.pm
+++ lib/XML/LibXML/SAX/Parser.pm
@@ -18,7 +18,7 @@ use XML::LibXML::Common qw(:libxml);
 use XML::SAX::Base;
 use XML::SAX::DocumentLocator;
 
-$VERSION = "2.0123"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0124"; # VERSION TEMPLATE: DO NOT CHANGE
 @ISA = ('XML::SAX::Base');
 
 sub CLONE_SKIP {
