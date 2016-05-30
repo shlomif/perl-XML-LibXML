e420c1aeff080c1ad5b70576037aac93c27bd61c
Shlomi Fish <shlomif@shlomifish.org>
2016-05-30 12:21:29 +0300

    Bump the version

--- lib/XML/LibXML/AttributeHash.pm
+++ lib/XML/LibXML/AttributeHash.pm
@@ -7,7 +7,7 @@ use Tie::Hash;
 our @ISA = qw/Tie::Hash/;
 
 use vars qw($VERSION);
-$VERSION = "2.0124"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0125"; # VERSION TEMPLATE: DO NOT CHANGE
 
 BEGIN
 {
