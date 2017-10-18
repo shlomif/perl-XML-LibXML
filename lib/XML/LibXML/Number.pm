e7ef85aba2a13408d84c33ff0021008b52d1173f
Shlomi Fish <shlomif@shlomifish.org>
2017-10-18 11:28:54 +0300

    bump version

--- lib/XML/LibXML/Number.pm
+++ lib/XML/LibXML/Number.pm
@@ -14,7 +14,7 @@ use strict;
 use warnings;
 
 use vars qw ($VERSION);
-$VERSION = "2.0129"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0130"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use overload
         '""' => \&value,
