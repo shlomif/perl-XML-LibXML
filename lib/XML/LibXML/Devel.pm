26917b22b150ca3296b0748fcc7952801cc2076f
Shlomi Fish <shlomif@shlomifish.org>
2019-02-02 13:09:27 +0200

    bump version

--- lib/XML/LibXML/Devel.pm
+++ lib/XML/LibXML/Devel.pm
@@ -12,7 +12,7 @@ use warnings;
 use XML::LibXML;
 
 use vars qw ($VERSION);
-$VERSION = "2.0132"; # VERSION TEMPLATE: DO NOT CHANGE
+$VERSION = "2.0133"; # VERSION TEMPLATE: DO NOT CHANGE
 
 use 5.008_000;
 
